dea8a01093f07a1c4617d05fbb8379ce6321510b
SpottieTea <nsdodson@miners.utep.edu>
2018-09-15 13:34:07 -0600

    Testing code for project

--- shell/shell.py
+++ shell/shell.py
@@ -1,4 +1,19 @@
 import sys        # command line arguments
+import time
 import re         # regular expression tools
 import os         # checking if file exists
 import subprocess # executing program
+
+ufile1 = sys.argv[1]
+ufile2 = sys.argv[2]
+
+print(ufile1)
+print(ufile2)
+
+
+pid = os.getpid()
+
+print(pid)
+
+
+
