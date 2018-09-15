4c54823ac9901005d2ff156327b308d80b8c2c05
SpottieTea <nsdodson@miners.utep.edu>
2018-09-15 15:16:26 -0600

    playing with forks and reading from console

--- shell/shell.py
+++ shell/shell.py
@@ -4,6 +4,9 @@ import re         # regular expression tools
 import os         # checking if file exists
 import subprocess # executing program
 
+
+#enter in bash in format: python3 shell.py [ufile1] [ufile2]
+
 ufile1 = sys.argv[1]
 ufile2 = sys.argv[2]
 
@@ -13,7 +16,18 @@ print(ufile2)
 
 pid = os.getpid()
 
-print(pid)
 
+forker = os.fork()
 
+if forker < 0:
 
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+
+elif forker == 0:                   # child
+
+        os.write(1, ("I am child.  Name==%s  Parent's pid=%d\n" % (ufile2, pid)).encode())
+
+else:                           # parent (forked ok)
+
+        os.write(1, ("I am parent.  Name=%s  Child's pid=%d\n" % (ufile1, forker)).encode())
