3a45aa754aec0128118ba7266e9d7ad000302957
SpottieTea <nsdodson@miners.utep.edu>
2018-09-17 20:30:13 -0600

    trying to make prompt loop; failing so far

--- shell/shell.py
+++ shell/shell.py
@@ -4,35 +4,37 @@ import re         # regular expression tools
 import os         # checking if file exists
 import subprocess # executing program
 
+inputuser = "none none"
+
+while inputuser != "exit":
+
+        inputuser = raw_input("Hello! Please provide two file names. \n")
+
+        print("Accepted names %s!" % inputuser)
+
+        inputuser = inputuser.split( )
+
+        print("Seperating files...")
+
+        ufile1 = inputuser[0]
+        ufile2 = inputuser[1]
+
+        print("Successfully seperated %s and %s!" % (ufile1, ufile2))
+
+        pid = os.getpid()
 
 
-inputuser = raw_input("Hello! Please provide two file names. \n")
+        forker = os.fork()
 
-print("Accepted names %s!" % inputuser)
+        if forker < 0:
 
-inputuser = inputuser.split( )
+                os.write(2, ("fork failed, returning %d\n" % forker).encode())
+                sys.exit(1)
 
-print("Seperating files...")
+        elif forker == 0:                   # child
 
-ufile1 = inputuser[0]
-ufile2 = inputuser[1]
+                os.write(1, ("I am child.  Name==%s  Parent's pid=%d\n" % (ufile2, pid)).encode())
 
-print("Successfully seperated %s and %s!" % (ufile1, ufile2))
+        else:                           # parent (forked ok)
 
-pid = os.getpid()
-
-
-forker = os.fork()
-
-if forker < 0:
-
-        os.write(2, ("fork failed, returning %d\n" % forker).encode())
-        sys.exit(1)
-
-elif forker == 0:                   # child
-
-        os.write(1, ("I am child.  Name==%s  Parent's pid=%d\n" % (ufile2, pid)).encode())
-
-else:                           # parent (forked ok)
-
-        os.write(1, ("I am parent.  Name=%s  Child's pid=%d\n" % (ufile1, forker)).encode())
+                os.write(1, ("I am parent.  Name=%s  Child's pid=%d\n" % (ufile1, forker)).encode())
